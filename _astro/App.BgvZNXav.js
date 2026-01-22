d12423169f53266bd76add41327ec3ba8d87d33d
shiozaki-new <shiozaki@newcom07.jp>
2026-01-22 16:58:42 +0900

    Delete _astro directory

--- _astro/App.BgvZNXav.js
+++
@@ -1,1 +0,0 @@
-import{j as t,D as o}from"./DashboardComplete.CCCZKuyQ.js";import"./index.BB7RSjL2.js";function s(){return t.jsx(o,{})}export{s as default};
--- _astro/DashboardComplete.Cw7C4orB.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./DashboardComplete.CCCZKuyQ.js";import"./index.BB7RSjL2.js";export{a as default};

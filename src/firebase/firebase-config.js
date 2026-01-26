54eb0be852c173048e6c28ea02cbfc96b22ca408
shiozaki-new <shiozaki@newcom07.jp>
2026-01-26 12:00:02 +0900

    Delete src directory

--- src/firebase/firebase-config.js
+++
@@ -1,9 +0,0 @@
-/* src/firebase/firebase-config.js */
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC1cVgoBleKcvXU3Z1G0FNY8JlL72pNP-c",
-  authDomain: "kikakugyoumu-907d6.firebaseapp.com",
-  projectId: "kikakugyoumu-907d6",
-  storageBucket: "kikakugyoumu-907d6.firebasestorage.app",
-  messagingSenderId: "259870871782",
-  appId: "1:259870871782:web:6cad7ff451d8cb5fdd6b27"
-};
